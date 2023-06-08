71fa37265266e6e4291d41d86f1cd0520517f3bd
j1t0 <rockypalafle@gmail.com>
2023-06-08 14:30:30 +0200

    END

--- public/js/winner.js
+++ public/js/winner.js
@@ -1,6 +1,5 @@
-
-const ganadores = JSON.parse(localStorage.getItem('ganadores')) || [];
+const ganadores = JSON.parse(localStorage.getItem("ganadores")) || [];
 if (ganadores.length > 0) {
-  const textarea = document.querySelector('.player');
-  textarea.value = ganadores.join('\n');
+  const textarea = document.querySelector(".player");
+  textarea.value = ganadores.join("\n");
 }
